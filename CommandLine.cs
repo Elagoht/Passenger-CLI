479c7ae23350620aaf3f2b26dc7cce15ad1a7f9f
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-05-04 18:20:56 +0300

    refactor: splitted classes

--- CommandLine.cs
+++ CommandLine.cs
@@ -2,70 +2,19 @@ namespace Passenger
 {
   public class CommandLine(string[] args)
   {
-    private readonly Authorization authorization = new(EnDeCoder.JSWSecret);
     private readonly string command = args[0];
-    private readonly string[] arguments = args.Skip(1).ToArray();
     public void Parse()
     {
       if (string.IsNullOrEmpty(command)) Error.MissingCommand();
-
+      Worker worker = new(args);
       switch (command)
       {
-        case "help" or "--help" or "-h": Help(); break;
-        case "login" or "-l": Login(); break;
-        case "register" or "-r": Register(); break;
-        case "reset" or "-R": Reset(); break;
+        case "help" or "--help" or "-h": Worker.Help(); break;
+        case "login" or "-l": worker.Login(); break;
+        case "register" or "-r": worker.Register(); break;
+        case "reset" or "-R": worker.Reset(); break;
         default: Error.UnknownCommand(); break;
       }
-    }
-    // Commands
-    private static void Help()
-    {
-      Console.Write(@"Passenger CLI 0.1.0
-  Copyright (C) 2024 Elagoht
-  
-  Store, retrieve, manage and generate passphrases securely using your own encode/decode algorithm. Every passenger client is created by user's itself and unique.
-
-Usage:
-  passenger [command] [*args]
-
-Commands:
-  login     -l [passphrase]      : generate a JWT token to use other commands
-  register  -r [passphrase]      : register a passphrase to the passenger client
-  reset     -R [jwt] [new]       : reset the passphrase of the passenger client
-  get       -g [jwt]             : list all entries without their passphrases
-  query     -q [jwt] [keyword]   : search for a keyword in all entries
-  save      -s [jwt] [data]      : store an entry with the given data
-  fetch     -f [jwt] [id]        : retrieve an entry by its id
-  update    -u [jwt] [id] [json] : update an entry by its id
-  delete    -d [jwt] [id]        : delete an entry by its index
-  help      -h --help            : show this help message and exit
-  man       -m                   : show the manual page, if available
-");
-      Environment.Exit(0);
-    }
-
-    private void Login()
-    {
-      if (arguments.Length != 1) Error.ArgumentCount("login", 1);
-      Console.WriteLine(authorization.GenerateToken(arguments[0]));
-      Environment.Exit(0);
-    }
-    private void Register()
-    {
-      if (arguments.Length != 1) Error.ArgumentCount("register", 1);
-      if (Database.IsRegistered())
-        Console.WriteLine("passenger: already registered");
-      else
-        Database.Register(arguments[0]);
-    }
-    private void Reset()
-    {
-      if (arguments.Length != 2) Error.ArgumentCount("reset", 2);
-      if (authorization.ValidateToken(arguments[0]))
-        Database.Register(arguments[1]);
-      else
-        Console.WriteLine("passenger: invalid token");
     }
   }
 
---
+++ Worker.cs
@@ -0,0 +1,57 @@
+namespace Passenger
+{
+  public class Worker(string[] args)
+  {
+    private readonly Authorization authorization = new(EnDeCoder.JSWSecret);
+    private readonly string[] arguments = args.Skip(1).ToArray();
+
+    public static void Help()
+    {
+      Console.Write(@"Passenger CLI 0.1.0
+  Copyright (C) 2024 Elagoht
+  
+  Store, retrieve, manage and generate passphrases securely using your own encode/decode algorithm. Every passenger client is created by user's itself and unique.
+
+Usage:
+  passenger [command] [*args]
+
+Commands:
+  login     -l [passphrase]      : generate a JWT token to use other commands
+  register  -r [passphrase]      : register a passphrase to the passenger client
+  reset     -R [jwt] [new]       : reset the passphrase of the passenger client
+  get       -g [jwt]             : list all entries without their passphrases
+  query     -q [jwt] [keyword]   : search for a keyword in all entries
+  save      -s [jwt] [data]      : store an entry with the given data
+  fetch     -f [jwt] [id]        : retrieve an entry by its id
+  update    -u [jwt] [id] [json] : update an entry by its id
+  delete    -d [jwt] [id]        : delete an entry by its index
+  help      -h --help            : show this help message and exit
+  man       -m                   : show the manual page, if available
+");
+      Environment.Exit(0);
+    }
+
+    public void Login()
+    {
+      if (arguments.Length != 1) Error.ArgumentCount("login", 1);
+      Console.WriteLine(authorization.GenerateToken(arguments[0]));
+      Environment.Exit(0);
+    }
+    public void Register()
+    {
+      if (arguments.Length != 1) Error.ArgumentCount("register", 1);
+      if (Database.IsRegistered())
+        Console.WriteLine("passenger: already registered");
+      else
+        Database.Register(arguments[0]);
+    }
+    public void Reset()
+    {
+      if (arguments.Length != 2) Error.ArgumentCount("reset", 2);
+      if (authorization.ValidateToken(arguments[0]))
+        Database.Register(arguments[1]);
+      else
+        Console.WriteLine("passenger: invalid token");
+    }
+  }
+}

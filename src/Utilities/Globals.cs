ea4a2acf3426fb75e24c70920316e20c6bebd323
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-02 18:14:16 +0300

    docs: Remove unnecessary comments in Globals.cs

--- src/Utilities/Globals.cs
+++ src/Utilities/Globals.cs
@@ -1,9 +1,5 @@
 namespace Passenger
 {
-  /// <summary>
-  /// Global constants
-  /// </summary>
-  /// <remarks>
   public static class GlobalConstants
   {
     public static readonly string VERSION = "0.1.0";

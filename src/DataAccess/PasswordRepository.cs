08265adf73dd636b25f8a974ff75b3a01087689b
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-12 21:07:56 +0300

    refactor: added static property to class & early returned function

--- src/DataAccess/PasswordRepository.cs
+++ src/DataAccess/PasswordRepository.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 namespace Passenger
 {
-  public class PasswordRepository
+  public static class PasswordRepository
   {
     public static List<string> Load()
     {
@@ -10,14 +10,11 @@ namespace Passenger
       var assembly = Assembly.GetExecutingAssembly();
       using (Stream stream = assembly.GetManifestResourceStream("Passenger.passwords.bin"))
       {
-        if (stream != null)
-        {
-          using var binaryReader = new BinaryReader(stream);
-          int count = binaryReader.ReadInt32();
-          for (int cursor = 0; cursor < count; cursor++)
-            passwordList.Add(binaryReader.ReadString());
-        }
-        else throw new Exception("Resource not found.");
+        if (stream == null) throw new Exception("Resource not found.");
+        using var binaryReader = new BinaryReader(stream);
+        int count = binaryReader.ReadInt32();
+        for (int cursor = 0; cursor < count; cursor++)
+          passwordList.Add(binaryReader.ReadString());
       }
       return passwordList;
     }

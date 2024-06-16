ad9fac10d327cc7742c63e1de3a8c96c03b74edc
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-06-16 13:50:20 +0300

    feat: trackable passphrase model added and used on database entry

--- src/DataAccess/Models.cs
+++ src/DataAccess/Models.cs
@@ -3,9 +3,9 @@ using System.Text.Json.Serialization;
 
 namespace Passenger
 {
+  /// <summary>Final model to use on database file</summary>
   public class DatabaseModel
   {
-
     [JsonPropertyName("username")]
     public string Username { get; set; }
     [JsonPropertyName("passphrase")]
@@ -16,6 +16,23 @@ namespace Passenger
     public List<ConstantPair> Constants { get; set; }
   }
 
+  /// <summary>Model to track passphrase changes over time</summary>
+  public class TrackablePassphrase
+  {
+    [JsonPropertyName("passphrase"), Required]
+    public string Passphrase { get; set; }
+    [JsonPropertyName("updated"), Required]
+    public string Updated { get; set; }
+  }
+
+  /// <summary>Model to use on JWT related operations</summary>
+  public class Credentials
+  {
+    public string Username { get; set; }
+    public string Passphrase { get; set; }
+  }
+
+  /// <summary>Key-value pair model for constants</summary>
   public class ConstantPair
   {
     [JsonPropertyName("key"), Required]
@@ -24,6 +41,7 @@ namespace Passenger
     public string Value { get; set; }
   }
 
+  /// <summary>Minimum details to use on statistics</summary>
   public class MicroDatabaseEntry
   {
     [JsonPropertyName("platform"), Required]
@@ -34,12 +52,14 @@ namespace Passenger
     public string Url { get; set; }
   }
 
+  /// <summary>Details to use on access counts logs</summary>
   public class CountableDatabaseEntry : MicroDatabaseEntry
   {
     [JsonPropertyName("totalAccesses"), Required]
     public int TotalAccesses { get; set; }
   }
 
+  /// <summary>Details to use on list of entries such as fetching all</summary>
   public class ListableDatabaseEntry : CountableDatabaseEntry
   {
     [JsonPropertyName("identity"), Required]
@@ -50,17 +70,12 @@ namespace Passenger
     public string Updated { get; set; }
   }
 
+  /// <summary>Maximum details to use on CRUD operations</summary>
   public class DatabaseEntry : ListableDatabaseEntry
   {
     [JsonPropertyName("passphrase"), Required]
-    public string Passphrase { get; set; }
+    public List<TrackablePassphrase> Passphrases { get; set; }
     [JsonPropertyName("notes")] // Optional
     public string Notes { get; set; }
-  }
-
-  public class Credentials
-  {
-    public string Username { get; set; }
-    public string Passphrase { get; set; }
   }
 }

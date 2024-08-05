184e7a1a78fa3002cd6abc8b4b83bef1be8d3f19
Onur Ravli <onur@onurravli.com>
2024-08-06 00:30:10 +0300

    chore: update version to 0.3.0-beta.1

--- src/Utilities/Globals.cs
+++ src/Utilities/Globals.cs
@@ -2,6 +2,6 @@ namespace Passenger
 {
   public static class GlobalConstants
   {
-    public static readonly string VERSION = "0.2.0";
+    public static readonly string VERSION = "0.3.0-beta.1";
   }
 }
